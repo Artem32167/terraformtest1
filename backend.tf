28d98282f5def22d30879401a1b6d45cab6ffb2b
Артем Бельков <blooddy@MacBook-Pro-Artem.local>
2025-08-27 14:12:26 +0200

    fix(backend): update backend config on mac

--- backend.tf
+++ backend.tf
@@ -1,9 +1,4 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstate23554"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    use_azuread_auth     = true
+  backend "local" {
   }
 }
